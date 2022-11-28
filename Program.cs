9893b1e2681148413bc2ed3cb8141dfbc333197c
abbas <faizi@student.chalmers.se>
2022-11-28 19:02:56 +0100

    the rocket and the plan

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Linq;
 
 namespace PingPong{
 
     class Program{
 
         static void Main(String[] args){
-            Console.WriteLine("Hello World Abbas");
-            Console.WriteLine("Hello World");
+            //  Console.WriteLine("Hello World Abbas");
+            //Console.WriteLine("Hello World");
+
+            const int fieldLengt = 50, fieldWidth = 15;
+            const char fieldTile = '#';
+            string line = string.Concat(Enumerable.Repeat(fieldTile, fieldLengt));
+
+
+
+            const int racketLength = fieldWidth / 4;
+            const char racketTile = '|';
+
+
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(line);
+
+                Console.SetCursorPosition(0, fieldWidth);
+                Console.WriteLine(line);
+
+
+                for(int i=0; i<racketLength; i++)
+                {
+                    Console.SetCursorPosition(0, i + 1);
+                    Console.WriteLine(racketTile);
+                    Console.SetCursorPosition(fieldLengt - 1, i + 1);
+                    Console.WriteLine(racketTile);
+                }
+            }
         }
     }
 }
