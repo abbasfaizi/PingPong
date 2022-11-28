e7139b79ae0be3c1a0994ca9512f692734b8c078
abbas <faizi@student.chalmers.se>
2022-11-28 15:28:15 +0100

    build the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PingPong{
+
+    class Program{
+
+        static void Main(String[] args){
+            Console.WriteLine("Hello World Abbas");
+            Console.WriteLine("Hello World");
+        }
+    }
+}
